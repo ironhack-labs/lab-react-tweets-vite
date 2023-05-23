4faf14b56d9b36e85184062c7c6338cfb75442c6
Raquel Barrio <r.barrio.f@gmail.com>
2023-05-23 09:53:24 +0200

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -5,7 +5,10 @@ import Message from "./Message";
 import Actions from "./Actions";
 
 function Tweet(props) {
-  // console.log("tweet:", props) //Es súper buena idea hacer aquí un console log para entender la estructura antes de destructurarlo.
+  // console.log("tweet:", props) // <-- Es súper buena idea hacer aquí un console log para entender la estructura antes de destructurarlo.
+
+  // si escribiese {tweet} en lugar de props estaría destructurando y declarando "tweet" como variable, y me ahorraría tener que escribir props en ninguna parte.
+
   const {image, name, handle} = props.tweet.user;
   const {timestamp, message} = props.tweet
 
