8d1490549b51fc84f7c1a6dedf0da6d1a1389422
Celia Angles <celiaangles22@gmail.com>
2023-11-27 18:47:24 +0100

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+//importing react, importing css of the index, importing the app where all the info comes from
+
 ReactDOM.createRoot(document.getElementById("root")).render(
+  //this means that the root of the app is gonna be printed and rendered inside the doccument as a react element? if it would say null nothing would appear
   <React.StrictMode>
     <App />
   </React.StrictMode>
