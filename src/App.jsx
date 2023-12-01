66e12d0e42be085f068b80e760dc8230c247bd3e
Ian <ianymkers@gmail.com>
2023-12-01 13:04:04 +0100

    Some issues still need to resolve

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,10 @@ function App() {
   return (
     <div className="App">
       <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
+      <Tweet tweet={tweetsArray[3]} />
     </div>
   );
 }
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,39 +1,33 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+
+function Tweet({user: {image, name, handle }, timestamp, message}) {
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+      <ProfileImage image={image} alt={name}/>
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
+        <div className="body">
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+          <div className="top">
+      <User name={name} handle={handle} />
+          </div>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
-      </div>
+      <Timestamp time={timestamp} />
 
-      <i className="fas fa-ellipsis-h"></i>
+      <Message message={message} />
+      <Actions />
+    </div>
+
+    <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
+
 
 export default Tweet;
