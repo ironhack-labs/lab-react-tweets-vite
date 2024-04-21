e41a40b602e25b6260713c0d733e2647f0c2431b
Ghada Bennasr <gh.bennasr@gmail.com>
2024-04-21 22:27:21 +0200

    lab solved

---
+++ src/components/Message.jsx
@@ -0,0 +1,8 @@
+function Message(props) {
+  return (
+    <div>
+      <p className="message">{props.message}</p>
+    </div>
+  );
+}
+export default Message;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,38 +1,32 @@
-function Tweet() {
+import Actions from "./Actions";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+
+function Tweet(props) {
   return (
-    <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+    <>
+      {props.tweet.map((prop, index) => (
+        <div key={index} className="tweet">
+          <ProfileImage image={prop.user.image} />
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <div className="body">
+            <div className="top">
+              <User name={prop.user.name} handle={prop.user.handle} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+              <Timestamp timestamp={prop.timestamp} />
+            </div>
+
+            <Message message={prop.message} />
+
+            <Actions />
+          </div>
+
+          <i className="fas fa-ellipsis-h"></i>
         </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
-      </div>
-
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
+      ))}
+    </>
   );
 }
 
