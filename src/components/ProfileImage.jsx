033eea44eb46e82a43974c2ba863fe82cfc1f493
monica am <monica.andres.consulting@gmail.com>
2024-10-09 12:00:00 +0200

    react

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,5 @@
+<img
+src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
+className="profile"
+alt="profile"
+/>
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,11 +1,7 @@
 function Tweet() {
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+
 
       <div className="body">
         <div className="top">
