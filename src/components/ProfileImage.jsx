206658e1678c74941a1468d4a47efb233a4b18ec
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-04 19:01:59 +0100

    lab react done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,8 @@
+function ProfileImage (){
+    return (
+        <div className="ProfileImage">
+        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/tweet-user-photo.png" className="profile" alt={`{user.name}'s profile`} />
+    </div>
+    )
+} 
+export default ProfileImage;
