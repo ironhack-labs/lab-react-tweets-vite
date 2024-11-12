d735d640d5c0a032e0dcd6096253cb0b6c7d088f
Mr Footwork <pandau.ting@gmail.com>
2024-11-12 12:17:08 +0100

    iteration 2: pass the tweet as a prop

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,39 +1,36 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+function Tweet({ tweet }) {
+	return (
+		<div className='tweet'>
+			<img
+				src={tweet.user.image}
+				className='profile'
+				alt='profile'
+			/>
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+			<div className='body'>
+				<div className='top'>
+					<span className='user'>
+						<span className='name'>{tweet.user.name}</span>
+						<span className='handle'>@{tweet.user.handle}</span>
+					</span>
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
+					<span className='timestamp'>{tweet.timestamp}</span>
+				</div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+				<p className='message'> {tweet.message} </p>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
-      </div>
+				<div className='actions'>
+					{/* Font Awesome icons */}
+					<i className='far fa-comment' data-testid='comment-icon'></i>
+					<i className='fas fa-retweet' data-testid='retweet-icon'></i>
+					<i className='far fa-heart' data-testid='heart-icon'></i>
+					<i className='fas fa-share' data-testid='share-icon'></i>
+				</div>
+			</div>
 
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
-  );
+			<i className='fas fa-ellipsis-h'></i>
+		</div>
+	);
 }
 
 export default Tweet;
